b96ff199f94248252e03eb951017a391dc16ea69
Rallenson <Rallenson900@gmail.com>
2023-04-22 14:33:38 -0300

    feat(variaveis)
    aprendendo a declarar variaveis

---
+++ PHP/Básico/Criando_Variaveis/index.php
@@ -0,0 +1,19 @@
+<?php
+    // string
+    $nome = "João";
+    echo $nome
+    // int
+    $idade = 20;
+    echo $idade
+    // float
+    $peso = 80.5;
+    echo $peso
+    // boolean
+    $casado = false;
+    echo $casado
+    // array
+    $filhos = null;
+    echo $filhos
+
+
+?>
