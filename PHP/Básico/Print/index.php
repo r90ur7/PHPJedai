c6f2c64bee79dde005bd0200dbcbf25d8c8f1f16
Rallenson <Rallenson900@gmail.com>
2023-04-22 14:01:19 -0300

    feat:ola mundo
    aprendendo estrutura base do php e sua sintaxe

---
+++ PHP/Básico/Print/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Printar mesnagens Na Tela</title>
+</head>
+<body>
+    <header>
+        <h1>Printar mensagens na tela</h1>
+    </header>
+    <main>
+            <?php
+                echo "Olá Mundo";
+            ?> 
+    </main>
+    <footer>
+        <p>Desenvolvido por <a href="https://github.com/r90ur7"/>
+            Rallenson Silva
+        </p>
+    </footer>
+</body>
+</html>
---
+++ PHP/Básico/Variaveis/index.php
@@ -0,0 +1,13 @@
+<?php
+    // Nome do dominio
+    echo $_SERVER['SERVER_NAME'];
+    echo "<br>";
+    // É o caminho até a pasta selecionada
+    echo $_SERVER['DOCUMENT_ROOT'];
+    echo "<br>";
+    // "Print_r" é uma função do PHP que mostra o que tem dentro de uma variável complexa.
+    echo '<pre>';
+    print_r($_SERVER);
+    echo '</pre>';
+    // SErver é um variavel que puxa todas as informações do servidor onde está
+?>
