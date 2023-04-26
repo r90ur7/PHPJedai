5ddb8c402d423478aa9e57ca0c139db92fd6c6df
Rallenson <Rallenson900@gmail.com>
2023-04-26 11:21:42 -0300

    entendendo loopings

---
+++ PHP/Básico/Loppings/index.php
@@ -0,0 +1,31 @@
+<?php
+//Loopings
+//for
+for($count=0;$count<11;$count++){
+    echo $count;
+    echo "<br>";
+}
+
+echo "<br/>";
+
+//while
+
+$contador = 0;
+while($contador<11){
+    echo $contador;
+    echo "<br>";
+    $contador++;
+}
+
+echo "<br/>";
+
+//do while
+
+$contador = 0;
+do{
+    echo $contador;
+    echo "<br>";
+    $contador++;
+}while($contador<11);
+
+?>
